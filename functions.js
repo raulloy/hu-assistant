c225274f075213f5d759088b8f4a0a8d507dc295
RaulLoyola <raul.loy@gmail.com>
2024-03-26 10:12:05 -0600

    Leads Generation

--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
 import { Client } from '@hubspot/api-client';
 import config from './config.js';
+import axios from 'axios';
 
 export const createHubSpotContact = async (
   firstname,
@@ -19,14 +20,53 @@ export const createHubSpotContact = async (
   };
 
   try {
-    const apiResponse = await hubspotClient.crm.contacts.basicApi.create({
-      properties,
+    const response = await axios({
+      url: `https://api.hubapi.com/crm/v3/objects/contacts/search`,
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        Authorization: `Bearer ${config.HUBSPOT_ACCESS_TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+      data: {
+        filterGroups: [
+          {
+            filters: [
+              {
+                propertyName: 'email',
+                operator: 'EQ',
+                value: email,
+              },
+            ],
+          },
+        ],
+        properties: [
+          'createdate',
+          'firstname',
+          'lastname',
+          'email',
+          'canal_de_captacion',
+          'sub_canal_de_captacion',
+          'desarrollo',
+          'lifecyclestage',
+        ],
+      },
     });
-    console.log(
-      'Contact created successfully.',
-      JSON.stringify(apiResponse, null, 2)
-    );
-    return apiResponse;
+    // console.log(response.data.results[0]?.properties.email);
+    const emailValidation = response.data.results[0]?.properties.email;
+
+    if (email !== emailValidation) {
+      const apiResponse = await hubspotClient.crm.contacts.basicApi.create({
+        properties,
+      });
+      console.log(
+        'Contact created successfully.',
+        JSON.stringify(apiResponse, null, 2)
+      );
+      return apiResponse;
+    } else {
+      console.log('Contact is already created');
+    }
   } catch (error) {
     console.error(
       `Failed to create contact: ${error.message}`,
@@ -35,3 +75,5 @@ export const createHubSpotContact = async (
     throw error;
   }
 };
+
+// createHubSpotContact('Raul', 'Loyola', '5541915469', 'raul@openai.com');
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import config from './config.js';
 import OpenAI from 'openai';
 import cors from 'cors';
+import { createHubSpotContact } from './functions.js';
 
 const app = express();
 
@@ -10,13 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 const port = config.PORT;
+
 const OPENAI_API_KEY = config.OPENAI_API_KEY;
 
 const openai = new OpenAI({
   apiKey: OPENAI_API_KEY,
 });
 
-app.get('/start', async (req, res) => {
+app.get('/api/start', async (req, res) => {
   try {
     console.log('Starting a new conversation...');
     const thread = await openai.beta.threads.create();
@@ -30,11 +32,15 @@ app.get('/start', async (req, res) => {
 
 app.post('/chat', async (req, res) => {
   try {
-    const { thread_id: threadId, message: userInput = '' } = req.body;
+    const data = req.body;
+    const threadId = data.thread_id;
+    const userInput = data.message || '';
+
     if (!threadId) {
       console.log('Error: Missing thread_id');
       return res.status(400).json({ error: 'Missing thread_id' });
     }
+
     console.log(`Received message: ${userInput} for thread ID: ${threadId}`);
 
     await openai.beta.threads.messages.create(threadId, {
@@ -42,53 +48,85 @@ app.post('/chat', async (req, res) => {
       content: userInput,
     });
 
+    // Run the Assistant
     const run = await openai.beta.threads.runs.create(threadId, {
       assistant_id: config.ASSISTANT_ID,
     });
 
-    console.log(`Run started with ID: ${run.id}`);
-    res.json({ run_id: run.id });
+    let runStatus;
+    do {
+      runStatus = await openai.beta.threads.runs.retrieve(threadId, run.id);
+
+      if (runStatus.status === 'requires_action') {
+        const args = JSON.parse(
+          runStatus.required_action.submit_tool_outputs.tool_calls[0].function
+            .arguments
+        );
+        const toolCallId =
+          runStatus.required_action.submit_tool_outputs.tool_calls[0].id;
+        const output = await createHubSpotContact(
+          args.firstname,
+          args.lastname,
+          args.phone,
+          args.email
+        );
+
+        // Check if 'output' is defined and handle accordingly
+        if (!output) {
+          await openai.beta.threads.runs.submitToolOutputs(threadId, run.id, {
+            tool_outputs: [
+              {
+                tool_call_id: toolCallId,
+                output: JSON.stringify({
+                  message: 'An error occurred, please try again.',
+                }),
+              },
+            ],
+          });
+        } else {
+          // Proceed to submit the output as before
+          await openai.beta.threads.runs.submitToolOutputs(threadId, run.id, {
+            tool_outputs: [
+              {
+                tool_call_id: toolCallId,
+                output: JSON.stringify(output),
+              },
+            ],
+          });
+        }
+      }
+    } while (runStatus.status !== 'completed');
+
+    const messages = await openai.beta.threads.messages.list(threadId);
+    const response = messages.data[0].content[0].text.value;
+
+    console.log(`Assistant response: ${response}`);
+    res.json({ response: response });
   } catch (error) {
     console.error(`Error in /chat endpoint: ${error.message}`);
     res.status(500).send('Error processing chat request');
   }
 });
 
-app.post('/check', async (req, res) => {
-  try {
-    const { thread_id: threadId, run_id: runId } = req.body;
-    if (!threadId || !runId) {
-      console.log('Error: Missing thread_id or run_id');
-      return res.status(400).json({ error: 'Missing thread_id or run_id' });
-    }
-
-    let endTime = Date.now() + 9000; // 9 seconds from now
-    let runStatus;
-
-    do {
-      runStatus = await openai.beta.threads.runs.retrieve(threadId, runId);
-      if (Date.now() > endTime) {
-        console.log('Run timed out');
-        return res.json({ response: 'timeout' });
-      }
-
-      if (runStatus.status === 'completed') {
-        const messages = await openai.beta.threads.messages.list(threadId);
-        const messageContent = messages.data[0].content[0].text;
-        console.log('Run completed, returning response');
-        return res.json({ response: messageContent, status: 'completed' });
-      }
-
-      if (runStatus.status === 'requires_action') {
-        // Process required actions here, similar to the Python code
-      }
-    } while (runStatus.status !== 'completed');
-  } catch (error) {
-    console.error(`Error in /check endpoint: ${error.message}`);
-    res.status(500).send('Error checking run status');
-  }
-});
-
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+// res.send(`
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8" />
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//   <title>Document</title>
+// </head>
+// <body>
+//   <h1>New thread created</h1>
+//   <script>
+//     window.dataLayer = window.dataLayer || [];
+//     dataLayer.push({'event': 'ChatBot_Lead'});
+//     console.log('Hello World!')
+//   </script>
+// </body>
+// </html>
+// `);
